137404a428bee79be5310b784e51c3adfc15015d
Captain Lugaru <72951616+CaptainLugaru@users.noreply.github.com>
2024-08-03 13:57:27 -0400

    added more actions;

--- src/main/java/me/lugaru/gaming/actions/Abilities.java
+++ src/main/java/me/lugaru/gaming/actions/Abilities.java
@@ -16,19 +16,37 @@ public class Abilities {
     boolean canTalk = true;
     boolean canFight = true;
     boolean canCheckQuest = true;
+    boolean canSteal = true;
+    boolean canBarter = true;
+    boolean canLoot = true;
+    boolean canPickLock = true;
+    boolean canWorldMove = true;
+    boolean canHack = true;
+    boolean canCraft = true;
+    boolean canScrap = true;
+    boolean canMove = true;
 
     public Abilities(
             Hero myhero) {
         thisHero = myhero;
     }
 
-    public Abilities(boolean canCheckInventory, boolean canCheckHunger, boolean canCheckThirst, boolean canCheckQuest, boolean canTalk, boolean canFight) {
+    public Abilities(boolean canCheckInventory, boolean canCheckHunger, boolean canCheckThirst, boolean canCheckQuest, boolean canTalk, boolean canFight, boolean canSteal, boolean canCraft, boolean canPickLock, boolean canWorldMove, boolean canHack, boolean canScrap, boolean canLoot, boolean canBarter, boolean canMove) {
         this.canCheckInventory = canCheckInventory;
         this.canCheckHunger = canCheckHunger;
         this.canCheckThirst = canCheckThirst;
         this.canTalk = canTalk;
         this.canFight = canFight;
         this.canCheckQuest = canCheckQuest;
+        this.canSteal = canSteal;
+        this.canBarter = canBarter;
+        this.canLoot = canLoot;
+        this.canScrap = canScrap;
+        this.canMove = canMove;
+        this.canWorldMove = canWorldMove;
+        this.canHack = canHack;
+        this.canCraft = canCraft;
+        this.canPickLock = canPickLock;
     }
 
     public void doAction(String actionMade) {
@@ -65,6 +83,52 @@ public class Abilities {
                     checkQuest();
                 }
                 break;
+                case "steal":
+                    if (canSteal) {
+                       steal();
+                    }
+                    break;
+                    case "barter":
+                        if (canBarter) {
+                            barter();
+                        }
+                        break;
+                        case "loot":
+                            if (canLoot) {
+                                looting();
+                            }
+                            break;
+                            case "craft":
+                                if (canCraft) {
+                                    craft();
+                                }
+                                break;
+                                case "scrap":
+                                    if (canScrap) {
+                                        scrap();
+                                    }
+                                    break;
+                                    case "move":
+                                        if (canMove) {
+                                            move();
+                                        }
+                                        break;
+            case "hack":
+                if (canHack) {
+                    hack();
+                }
+                break;
+                    case "world move":
+                        if (canWorldMove) {
+                            worldMove();
+                        }
+                        break;
+            case "lock picking":
+                if (canPickLock) {
+                    lockPicking();
+                }
+                break;
+
             case "end":
                 System.out.println("Ending action.");
                 break;
@@ -94,6 +158,34 @@ public class Abilities {
         }
         if (canFight) {
             System.out.println("Fight");
+        }
+        if (canSteal) {
+            System.out.println("Steal");
+        }
+        if (canBarter) {
+            System.out.println("Barter");
+        }
+        if (canLoot) {
+            System.out.println("Loot");
+        }
+        if (canCraft) {
+            System.out.println("Craft");
+        }
+        if (canScrap) {
+            System.out.println("Scrap");
+        }
+        if (canMove) {
+            System.out.println("Move");
+        }
+        if (canWorldMove) {
+            System.out.println("World Move");
+        }
+        if (canHack) {
+            System.out.println("Hack");
+
+        }
+        if (canPickLock){
+            System.out.println("Pick Lock");
         }
     }
 
@@ -136,4 +228,32 @@ public class Abilities {
     public void fight() {
         System.out.println("Fighting...");
     }
+    public void steal(){
+        System.out.println("Stealing...");
+    }
+    public void barter() {
+        System.out.println("Bartering...");
+    }
+    public void craft() {
+        System.out.println("Craft...");
+    }
+    public void scrap() {
+        System.out.println("Scraping...");
+    }
+    public void move() {
+        System.out.println("Moving...");
+    }
+    public void hack() {
+        System.out.println("Hacking...");
+    }
+    public void worldMove(){
+        System.out.println("Traveling far");
+    }
+    public void lockPicking(){
+        System.out.println("Locking picking...");
+    }
+    public void looting(){
+        System.out.println("Looting");
+    }
 }
+
