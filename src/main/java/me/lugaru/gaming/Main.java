96a6c357e323cee7ef4d870774384607a52437df
Captain Lugaru <72951616+CaptainLugaru@users.noreply.github.com>
2024-08-05 13:49:42 -0400

    Added a lot to map and made the game looped.

--- src/main/java/me/lugaru/gaming/Main.java
+++ src/main/java/me/lugaru/gaming/Main.java
@@ -3,17 +3,30 @@ package me.lugaru.gaming;
 import me.lugaru.gaming.actions.Dialogue;
 import me.lugaru.gaming.character.Hero;
 import me.lugaru.gaming.character.PointSystem;
+import me.lugaru.gaming.world.WorldMap;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         Dialogue dialogue = new Dialogue();
         Hero playerCharacter = new Hero("Soul Survivor", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         dialogue.startGameMethodDialogue();
         dialogue.summaryMethod();
         playerCharacter.pointSystem.gameMethodStart(scanner);
+       playerCharacter.worldMap.initializeMap();
+        playerCharacter.worldMap.setCurrentPlace("Home Village");
+
+        while (true){
+
+            playerCharacter.abilities.showActions();
+            playerCharacter.abilities.doAction(scanner.nextLine());
+            if( playerCharacter.hp < 1){
+               break;
+            }
+        }
     }
 }//
