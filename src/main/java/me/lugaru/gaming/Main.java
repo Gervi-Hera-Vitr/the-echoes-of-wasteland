1a96b7d86c7e07da501d78a4125d1c13b04411f3
Captain Lugaru <72951616+CaptainLugaru@users.noreply.github.com>
2024-08-13 20:21:22 -0400

    added items to inventory

--- src/main/java/me/lugaru/gaming/Main.java
+++ src/main/java/me/lugaru/gaming/Main.java
@@ -3,6 +3,8 @@ package me.lugaru.gaming;
 import me.lugaru.gaming.actions.Dialogue;
 import me.lugaru.gaming.character.Hero;
 import me.lugaru.gaming.character.PointSystem;
+import me.lugaru.gaming.inventory.Armor;
+import me.lugaru.gaming.inventory.Item;
 import me.lugaru.gaming.world.WorldMap;
 
 import java.util.Scanner;
@@ -10,6 +12,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Item bottleCaps = new Item("Bottle caps", 1);
+        Armor Leather_Helmet = new Armor("Leather Helmet", 2, 1, 1);
 
         Scanner scanner = new Scanner(System.in);
         Dialogue dialogue = new Dialogue();
@@ -19,8 +23,11 @@ public class Main {
         playerCharacter.pointSystem.gameMethodStart(scanner);
         playerCharacter.worldMap.initializeMap();
         playerCharacter.worldMap.setCurrentPlace("Home Village");
+        playerCharacter.inventory.addItem(bottleCaps);
+        playerCharacter.inventory.addItem(Leather_Helmet); 
         do {
             playerCharacter.worldMap.initializeMap();
+            playerCharacter.abilities.showActions();
             playerCharacter.abilities.doAction(scanner.nextLine());
         } while (playerCharacter.hp < 1);
 
--- src/main/java/me/lugaru/gaming/inventory/Armor.java
+++ src/main/java/me/lugaru/gaming/inventory/Armor.java
@@ -2,9 +2,11 @@ package me.lugaru.gaming.inventory;
 
 public class Armor extends Item {
     int protection;
-
-    public Armor(String name, int value, int protection) {
+    int slot;
+    public Armor(String name, int value, int protection, int slot) {
         super(name, value);
         this.protection = protection;
+        this.slot = slot;
     }
+
 }
