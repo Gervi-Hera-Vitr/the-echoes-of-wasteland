6abead6ae9eabdfb5db8d231686271ec97976773
rdd13r <12781006+rdd13r@users.noreply.github.com>
2024-11-07 08:41:10 -0500

    Added comments.

--- docs/_plugins/expand_nested_variable_filter.rb
+++ docs/_plugins/expand_nested_variable_filter.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 module Jekyll
+
+  # A Liquid filter that flattens nested variables in the given input string.
+  #
+  # @param input [String] a string that may contain nested Liquid variables
+  # @return [String] the input string with all nested variables flattened
   module ExpandNestedVariableFilter
     # Flattens nested variables in the given input string.
     #
