b31a4bee76c052880e78f50f726a7bbd8b9afc2d
R!dd13r <12781006+rdd13r@users.noreply.github.com>
2024-11-08 03:30:14 -0500

    10 correct all of the security issues on the echoes of wasteland fix workflow names (#12)
    * Fixed badge labels
    
    * Corrected Gemfile and dependencies
    
    * Correcting code scan triggers
    
    * Pushing renovate configuration
    
    * Styling fixes
    
    * Added comments.
    
    * Added scan to gradle.
    
    * Corrected indents.
    
    * Added editor config.
    
    * Configured editor and buildScan
    
    * Build fix.
    
    ---------
    
    Signed-off-by: R!dd13r <12781006+rdd13r@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,36 +1,53 @@
 @file:Suppress("UnstableApiUsage")
 
 pluginManagement {
-    val useJavaVer: String by settings
+  val useJavaVer: String by settings
 
-    val jupiterVersion: String by settings
-    val assertjVersion: String by settings
-    val jacocoToolVersion: String by settings
+  val jupiterVersion: String by settings
+  val assertjVersion: String by settings
+  val jacocoToolVersion: String by settings
 
-    val toolchainsFoojayResolverVersion: String by settings
-    val ioFreeFairVersion: String by settings
+  val toolchainsFoojayResolverVersion: String by settings
+  val ioFreeFairVersion: String by settings
 
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-        google()
-    }
+  val develocityVersion: String by settings
+
+  repositories {
+    gradlePluginPortal()
+    mavenCentral()
+    google()
+  }
 
 //      https://docs.gradle.org/current/userguide/plugins.html
 //      https://plugins.gradle.org/
-    plugins {
+  plugins {
 
 //      https://docs.gradle.org/current/userguide/application_plugin.html
-        application
+    application
 
 //      https://docs.gradle.org/current/userguide/jacoco_plugin.html
-        jacoco
+    jacoco
 
 //        https://docs.freefair.io/gradle-plugins/current/reference/
-        id("io.freefair.lombok") version ioFreeFairVersion
+    id("io.freefair.lombok") version ioFreeFairVersion
 
 
 //        https://docs.gradle.org/current/userguide/toolchains.html
-        id("org.gradle.toolchains.foojay-resolver-convention") version toolchainsFoojayResolverVersion
-    }
+    id("org.gradle.toolchains.foojay-resolver-convention") version toolchainsFoojayResolverVersion
+
+    id("com.gradle.develocity") version develocityVersion
+  }
+}
+
+plugins {
+  id("com.gradle.develocity")
+}
+
+//        https://docs.gradle.com/develocity/gradle-plugin/current/
+develocity {
+  buildScan {
+    termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+    termsOfUseAgree.set("yes")
+    publishing.onlyIf { it.buildResult.failures.isNotEmpty() }
+  }
 }
